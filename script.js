2091e3ab023e4acac0b81e6f01b13879dab9a6e8
Adz-Nelo <adz.nelo155@gmail.com>
2025-11-02 00:02:26 +0800

    add sliding effect img

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ document.querySelector('#menu-btn').onclick = () => {
 window.onscroll = () => {
     navbar.classList.toggle('active')
 }
+
+let swiper = new Swiper(".home-slider", {
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+})
