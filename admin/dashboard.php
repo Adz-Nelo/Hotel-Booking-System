67115985da140dc750553fbc83dc7ebff547f27f
Adz-Nelo <adz.nelo155@gmail.com>
2025-11-23 22:32:38 +0800

    dashboard issue

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+
+    <link rel="stylesheet" href="../css/admin-style.css">
+    
 </head>
 <body>
-    
+    <!-- header section starts -->
+    <?php include("../components/admin_header.php"); ?>
+    <!-- header section ends -->
+
+    <script src="../js/admin_script.js" type="text/javascript"></script>
 </body>
 </html>
