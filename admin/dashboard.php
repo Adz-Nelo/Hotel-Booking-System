a78d0e8525e450f73683648668dc09cf6f313a56
Adz-Nelo <adz.nelo155@gmail.com>
2025-11-24 23:03:27 +0800

    fix header bug

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
 
-    <link rel="stylesheet" href="../css/admin-style.css">
-    
+    <link rel="stylesheet" href="../css/admin_style.css">     
 </head>
 <body>
     <!-- header section starts -->
